a0a9cdceb412b807242b8ccdf0bab0961e7b5353
busarihamzat <busaribilikis1@gmail.com>
2024-09-05 07:48:59 +0100

    frontend 2

---
+++ script.js
@@ -0,0 +1,27 @@
+let number = prompt("Please enter a number:");
+number = Number(number);
+
+if (number > 0) {
+    alert(1);
+} else if (number < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+let u = 0;
+while (u <= 10) {
+    console.log(u);
+    u++;
+}
+
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i <= 10);
